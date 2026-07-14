63958de11d9e5a0f8b302def04817850b4e335d9
IliutaAndrei <andreiiliuta586@yahoo.com>
2026-07-14 12:18:10 +0300

    billboard service finished

---
+++ constans.py
@@ -0,0 +1,7 @@
+BILLBOARD_TOP100_URL = "https://www.billboard.com/charts/hot-100/"
+
+TITLE_CSS_SELECTOR = "h3#title-of-a-story"
+
+ARTIST_CSS_SELECTOR = "span.c-label > a"
+
+SONG_LIST_CSS_SELECTOR = "ul.o-chart-results-list-row"
---
+++ services/billboard_service.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup, Tag
+import requests
+
+from constans import TITLE_CSS_SELECTOR, ARTIST_CSS_SELECTOR, SONG_LIST_CSS_SELECTOR
+
+
+def _extract_song_details(element: Tag) -> dict[str, str] | None:
+    title_element = element.select_one(TITLE_CSS_SELECTOR)
+    artist_element = element.select_one(ARTIST_CSS_SELECTOR)
+
+    if not title_element or not artist_element:
+        return None
+
+    title = title_element.get_text(strip=True)
+    artist = artist_element.get_text(strip=True)
+
+    return {
+        "title": title,
+        "artist": artist
+    }
+
+def _get_html_from_url(link: str) -> str:
+    page = requests.get(link)
+    page.raise_for_status()
+
+    return page.text
+
+
+def get_songs(url: str) -> list[dict[str, str]]:
+    songs = []
+
+    html = _get_html_from_url(url)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    song_list = soup.select(SONG_LIST_CSS_SELECTOR)
+
+    for song in song_list:
+        song_details = _extract_song_details(song)
+        if song_details is not None:
+            songs.append(song_details)
+
+    return songs
+
+
+
+
